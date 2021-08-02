9da733f2aeaf4098434201db7afb391c39d3593c
Angel Almanza <almanza.angel245@gmail.com>
2021-08-01 19:06:34 -0600

    I corrected errors, I created the list of words, and I added a function for choose one word

--- main.py
+++ main.py
@@ -1,29 +1,40 @@
-# import random
+import random
 import os
 
 
-# def chooseWord():
-#     index = random.randint(1, 171)
+# Con esto creo la lista de palabras para el juego
+def getListOfWords():
+    with open("./files/data.txt", "r", encoding="utf-8") as f:
+        return  [i for i in f]
+
+
+def chooseWord():
+    return random.randint(1, 172)
 
 
 def chooseDifficulty(length):
+    # Pido que el jugador elija una dificultad
     difficulty = input("1.Easy\n2.Normal\n3.Hard\n")
+    # Me aseguro de que la dificultad sea valida
     assert difficulty.isnumeric(), "Excuse us, the chosen difficulty doesn't exist"
     difficulty = int(difficulty)
     if difficulty == 3:
-        return length + 3
-    elif difficulty == 2:
-        return length + 6
-    else:
         return length * 2
+    elif difficulty == 2:
+        return length * 3
+    else:
+        return length * 4
 
 
 def run():
     playing = 1
+    words = getListOfWords()
+
     while True:
         print("Welcome to El Ahorcado\nChoose a difficulty")
-        attempts = chooseDifficulty(6)
-        print(attempts)
+        index = chooseWord()
+        word = words[index]
+        attempts = chooseDifficulty(len(word))
         playing = int(input("Do you want to playing yet ? \n0.No \n1.Si\n"))
         if playing != 1:
             break
